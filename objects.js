7d3bf035701cffd9bd1105e435ff5db91d6ec9dd
Jon Caneiro <joncaneiro@gmail.com>
2019-07-30 00:00:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,13 @@
-var playlist
+var playlist = {Black keys: 'Hi/lo'}, {Nirvana: 'Come as you are'}, {RHCP: 'Look around'}
 
-Object.assign({}, {Black keys: 'Hi/lo'}, {Nirvana: 'Come as you are'}, {RHCP: 'Look around'})
+function updatePlaylist(obj, key, value )
 
-function updatePlaylist(obj, )
+Object.assign({}, [key]: value)
+
+return obj
+}
+
+function removeFromPlaylist(obj, key) {
+  delete obj.key
+  return obj
+}
