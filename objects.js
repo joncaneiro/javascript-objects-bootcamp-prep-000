359be9a5f76cd5ee096dcb478bddba41a29f956e
Jon Caneiro <joncaneiro@gmail.com>
2019-07-30 00:18:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
-  if true {
   return playlist
-  }
 }
