1218ceb5cbb680a6afa38cafd34545c9300376b8
Jon Caneiro <joncaneiro@gmail.com>
2019-07-30 00:11:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,13 +1,2 @@
 var playlist = {Black keys: 'Hi/lo'}
 
-function updatePlaylist(playlist, key, value ) {
-
-      playlist.key = 'value'
-
-return playlist
-}
-
-function removeFromPlaylist(playlist, key) {
-  delete playlist.key
-  return playlist
-}
